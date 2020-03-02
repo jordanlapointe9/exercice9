567e9df7aaa70939f4534dd482ffca3b1e2f8e74
Jordan Lapointe <jordan.lapointe87@gmail.com>
2020-03-02 09:38:52 -0500

    Modification du header et du footer

--- footer.php
+++ footer.php
@@ -15,17 +15,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tp2-veille' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tp2-veille' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tp2-veille' ), 'tp2-veille', '<a href="http://underscores.me/">Thiago Ferreira, Jordan Lapointe</a>' );
-				?>
+			<a href='<?php get_home_url() ?>'>Exercice #9</a> &mdash;
+			<p>&mdash; Créé par Jordan Lapointe</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -16,13 +16,13 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tp2-veille' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -45,7 +45,6 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tp2-veille' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
