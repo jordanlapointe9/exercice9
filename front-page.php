a1000b15ba4169d5743169a9f150f92a7de69320
Jordan Lapointe <jordan.lapointe87@gmail.com>
2020-03-02 10:32:06 -0500

    Design de base complet (header, navigation et footer)

--- front-page.php
+++ front-page.php
@@ -2,9 +2,17 @@
 //Appel du header
 get_header();
 
-//Appel de la fonction qui affiche l'image de mise en avant
-tp2_veille_post_thumbnail();
+?>
+<!-- Figure qui contrôle le cadre de l'image de mise en avant -->
+<figure class="figure-post-thumbnail">
+    
+    <?php 
+    //Appel de la fonction qui affiche l'image de mise en avant
+    tp2_veille_post_thumbnail(); 
+    ?>
+</figure>
 
+<?php
 //Appel du footer
 get_footer();
 ?>
