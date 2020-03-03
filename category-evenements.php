80ac985cd4f9669244df6707490f5ad353ab7b53
Jordan Lapointe <jordan.lapointe87@gmail.com>
2020-03-03 13:59:34 -0500

    Version finale

--- category-evenements.php
+++ category-evenements.php
@@ -6,11 +6,20 @@
     if ( have_posts() ) :
         echo '<div class="container-evenements">';
             while ( have_posts() ) : the_post();
+            $jour = (int) get_the_date('j');
+                $mois = (int) get_the_date('n');
+                $jourGrid = get_the_date('j') + 1;
+                $moisGrid = convertMoisEnColone($mois);
+                $gridArea = ''.$jour.'/'.$mois.'/'.$jourGrid.'/'.$moisGrid.'';
+
             echo '
-            <article class="articles-evenements" style="">
+            <article class="articles-evenements" style="
+                grid-area: '.$gridArea.';
+            ">
                 <div class="content-post">
                     <h3 class="title-article"><a target=_blank href='.get_the_permalink().'>'.substr(get_the_title(), 0, 20).'</a></h3>
                     <h4 class=post-date>'.get_the_date().'</h4>
+                    <p>Grid : '.$gridArea.'</p>
                 </div>
             </article>
             ';
@@ -19,4 +28,21 @@
     endif;
 
     get_footer();
+
+    function convertMoisEnColone($value){
+        switch ($value) {
+            case 9:
+                return 1;
+                break;
+            case 10:
+                return 2;
+                break;
+            case 11:
+                return 3;
+                break;
+            default: 
+                return 0;
+                break;
+        }
+    }
 ?>
